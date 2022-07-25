4d689ecdcfb08b2e172ef8fbe02f5c6649cbad2a
John Gargalionis <johngargalionis@gmail.com>
2022-07-25 17:43:37 +0200

    Fixed failing default cli tests

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -1,4 +1,5 @@
 import pytest
+import click
 from click.testing import CliRunner
 from feynwrite import cli
 
@@ -8,22 +9,8 @@ def runner():
     return CliRunner()
 
 
-def test_cli(runner):
-    result = runner.invoke(cli.main)
-    assert result.exit_code == 0
-    assert not result.exception
-    assert result.output.strip() == 'Hello, world.'
-
-
-def test_cli_with_option(runner):
-    result = runner.invoke(cli.main, ['--as-cowboy'])
-    assert not result.exception
-    assert result.exit_code == 0
-    assert result.output.strip() == 'Howdy, world.'
-
-
 def test_cli_with_arg(runner):
-    result = runner.invoke(cli.main, ['John'])
+    result = runner.invoke(cli.main, ["John"])
     assert result.exit_code == 0
     assert not result.exception
-    assert result.output.strip() == 'Hello, John.'
+    assert result.output.strip() == "Hello, John."
