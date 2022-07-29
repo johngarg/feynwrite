d7ce83b352e5259473423d84ff2a2e74bec22470
John Gargalionis <johngargalionis@gmail.com>
2022-07-29 11:05:14 +0200

    Added more dependencies to setup.py

--- setup.py
+++ setup.py
@@ -3,46 +3,42 @@ Automate the production of FeynRules files.
 """
 from setuptools import find_packages, setup
 
-dependencies = ['click']
+dependencies = ["click", "matchmakereft", "rich"]
 
 setup(
-    name='feynwrite',
-    version='0.1.0',
-    url='https://github.com/johngarg/feynwrite',
-    license='BSD',
-    author='John Gargalionis',
-    author_email='johngargalionis@gmail.com',
-    description='Automate the production of FeynRules files.',
+    name="feynwrite",
+    version="0.1.0",
+    url="https://github.com/johngarg/feynwrite",
+    license="BSD",
+    author="John Gargalionis",
+    author_email="johngargalionis@gmail.com",
+    description="Automate the production of FeynRules files.",
     long_description=__doc__,
-    packages=find_packages(exclude=['tests']),
+    packages=find_packages(exclude=["tests"]),
     include_package_data=True,
     zip_safe=False,
-    platforms='any',
+    platforms="any",
     install_requires=dependencies,
-    entry_points={
-        'console_scripts': [
-            'feynwrite = feynwrite.cli:main',
-        ],
-    },
+    entry_points={"console_scripts": ["feynwrite = feynwrite.cli:main",],},
     classifiers=[
         # As from http://pypi.python.org/pypi?%3Aaction=list_classifiers
         # 'Development Status :: 1 - Planning',
         # 'Development Status :: 2 - Pre-Alpha',
         # 'Development Status :: 3 - Alpha',
-        'Development Status :: 4 - Beta',
+        "Development Status :: 4 - Beta",
         # 'Development Status :: 5 - Production/Stable',
         # 'Development Status :: 6 - Mature',
         # 'Development Status :: 7 - Inactive',
-        'Environment :: Console',
-        'Intended Audience :: Developers',
-        'License :: OSI Approved :: BSD License',
-        'Operating System :: POSIX',
-        'Operating System :: MacOS',
-        'Operating System :: Unix',
-        'Operating System :: Microsoft :: Windows',
-        'Programming Language :: Python',
-        'Programming Language :: Python :: 2',
-        'Programming Language :: Python :: 3',
-        'Topic :: Software Development :: Libraries :: Python Modules',
-    ]
+        "Environment :: Console",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: BSD License",
+        "Operating System :: POSIX",
+        "Operating System :: MacOS",
+        "Operating System :: Unix",
+        "Operating System :: Microsoft :: Windows",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 3",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+    ],
 )
